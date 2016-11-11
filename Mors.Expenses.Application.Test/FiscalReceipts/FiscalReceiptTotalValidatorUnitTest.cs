69bb3b35433a52e8a1418dea6ccfd5484b243e3e
Łukasz Mrozek <morskk@gmail.com>
2016-11-11 21:47:52 +0100

    Added FiscalReceiptTotalValidator

---
+++ Mors.Expenses.Application.Test/FiscalReceipts/FiscalReceiptTotalValidatorUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using Mors.Expenses.Application.FiscalReceipts;
+using Mors.Expenses.Data.Commands.Dtos;
+using Xunit;
+
+namespace Mors.Expenses.Application.Test.FiscalReceipts
+{
+    public class FiscalReceiptTotalValidatorUnitTest
+    {
+        [Fact]
+        public void Validate_Returns_True_For_Valid_Total()
+        {
+            Assert.True(FiscalReceiptTotalValidator.Validate(ValidTotal()));
+        }
+
+        [Theory]
+        [MemberData("InvalidTotals")]
+        public void Validate_Returns_False_For_Invalid_Total(FiscalReceiptTotal invalidTotal)
+        {
+            Assert.False(FiscalReceiptTotalValidator.Validate(invalidTotal));
+        }
+
+        private static FiscalReceiptTotal ModifyValidTotal(Action<FiscalReceiptTotal> modify)
+        {
+            var total = ValidTotal();
+            modify(total);
+            return total;
+        }
+
+        public static IEnumerable InvalidTotals()
+        {
+            yield return Row(default(FiscalReceiptTotal));
+            yield return Row(ModifyValidTotal(t => { t.VatRateLetter = null; }));
+            yield return Row(ModifyValidTotal(t => { t.VatRateLetter = ""; }));
+            yield return Row(ModifyValidTotal(t => { t.VatRateLetter = " "; }));
+            yield return Row(ModifyValidTotal(t => { t.VatRatePercentValue = -4m; }));
+        }
+
+        private static IEnumerable Row(params object[] parameters)
+        {
+            return parameters;
+        }
+
+        private static FiscalReceiptTotal ValidTotal()
+        {
+            return new FiscalReceiptTotal
+            {
+                GrossValue = 20.48m,
+                VatRateLetter = "D",
+                VatRatePercentValue = 23m,
+                VatValue = 3.83m
+            };
+        }
+    }
+}
---
+++ Mors.Expenses.Application/FiscalReceipts/FiscalReceiptTotalValidator.cs
@@ -0,0 +1,20 @@
+﻿using Mors.Expenses.Data.Commands.Dtos;
+
+namespace Mors.Expenses.Application.FiscalReceipts
+{
+    internal static class FiscalReceiptTotalValidator
+    {
+        public static bool Validate(FiscalReceiptTotal total)
+        {
+            if (total == null)
+            {
+                return false;
+            }
+            if (total.VatRatePercentValue < 0m)
+            {
+                return false;
+            }
+            return !string.IsNullOrWhiteSpace(total.VatRateLetter);
+        }
+    }
+}

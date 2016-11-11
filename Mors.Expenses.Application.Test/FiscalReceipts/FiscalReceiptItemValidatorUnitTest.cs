7dc1beeae0d6a3f5d0cc4fcba2f2c2938015282b
Łukasz Mrozek <morskk@gmail.com>
2016-11-11 21:00:58 +0100

    Added validator for FiscalReceiptItem

---
+++ Mors.Expenses.Application.Test/FiscalReceipts/FiscalReceiptItemValidatorUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Mors.Expenses.Application.FiscalReceipts;
+using Mors.Expenses.Data.Commands.Dtos;
+using Xunit;
+
+namespace Mors.Expenses.Application.Test.FiscalReceipts
+{
+    public class FiscalReceiptItemValidatorUnitTest
+    {
+        [Fact]
+        public void Validate_Returns_True_For_Valid_Receipt_Item()
+        {
+            Assert.True(FiscalReceiptItemValidator.Validate(ValidReceiptItem()));
+        }
+
+        private static FiscalReceiptItem ValidReceiptItem()
+        {
+            return new FiscalReceiptItem
+            {
+                Amount = 0.574m,
+                GrossValue = 9.18m,
+                Name = "D_FILET Z KURCH.*2",
+                UnitGrossValue = 15.99m,
+                VatRateLetter = "D"
+            };
+        }
+    }
+}
---
+++ Mors.Expenses.Application/FiscalReceipts/FiscalReceiptItemValidator.cs
@@ -0,0 +1,12 @@
+﻿using Mors.Expenses.Data.Commands.Dtos;
+
+namespace Mors.Expenses.Application.FiscalReceipts
+{
+    internal static class FiscalReceiptItemValidator
+    {
+        public static bool Validate(FiscalReceiptItem item)
+        {
+            return true;
+        }
+    }
+}

463bb0e2432a7c75cfc793da8c74c4256dcd375d
Łukasz Mrozek <morskk@gmail.com>
2016-12-04 17:52:50 +0100

    Added command validation to AddFiscalReceiptCommandHandler

--- Mors.Expenses.Application.Test/FiscalReceipts/AddFiscalReceiptCommandHandlerIntegrationTest.cs
+++ Mors.Expenses.Application.Test/FiscalReceipts/AddFiscalReceiptCommandHandlerIntegrationTest.cs
@@ -1,8 +1,47 @@
-﻿using Xunit;
+﻿using System.Collections;
+using System.Linq;
+using Mors.Expenses.Application.FiscalReceipts;
+using Mors.Expenses.Data.Commands;
+using Xunit;
 
 namespace Mors.Expenses.Application.Test.FiscalReceipts
 {
     public sealed class AddFiscalReceiptCommandHandlerIntegrationTest
     {
+        [Theory]
+        [MemberData("InvalidCommands")]
+        public void Execute_Returns_False_Given_Invalid_Command(AddFiscalReceiptCommand command)
+        {
+            var handler = Handler();
+            var result = handler.Execute(command, null);
+            Assert.Equal(false, result);
+        }
+
+        [Fact]
+        public void Execute_Returns_True_Given_Valid_Command()
+        {
+            var handler = Handler();
+            var result = handler.Execute(ValidCommand(), null);
+            Assert.Equal(true, result);
+        }
+
+        public static IEnumerable InvalidCommands()
+        {
+            yield return new[] { default(AddFiscalReceiptCommand) };
+            foreach (var invalidReceipt in FiscalReceiptTestData.InvalidReceipts())
+            {
+                yield return new[] { new AddFiscalReceiptCommand { Receipt = invalidReceipt } };
+            }
+        }
+
+        private static AddFiscalReceiptCommand ValidCommand()
+        {
+            return new AddFiscalReceiptCommand{ Receipt = FiscalReceiptTestData.ValidReceipt() };
+        }
+
+        private static AddFiscalReceiptCommandHandler Handler()
+        {
+            return new AddFiscalReceiptCommandHandler();
+        }
     }
 }
--- Mors.Expenses.Application/FiscalReceipts/AddFiscalReceiptCommandHandler.cs
+++ Mors.Expenses.Application/FiscalReceipts/AddFiscalReceiptCommandHandler.cs
@@ -1,9 +1,16 @@
-﻿namespace Mors.Expenses.Application.FiscalReceipts
+﻿using Mors.Expenses.Data.Commands;
+
+namespace Mors.Expenses.Application.FiscalReceipts
 {
     internal sealed class AddFiscalReceiptCommandHandler
     {
-        public void Execute(ICommandEnvironment environment)
+        public bool Execute(AddFiscalReceiptCommand command, ICommandEnvironment environment)
         {
+            if (command == null || !FiscalReceiptValidator.Validate(command.Receipt))
+            {
+                return false;
+            }
+            return true;
         }
     }
 }

38e65b4ae15acb431eab2e304eed2c2db5e9834f
Łukasz Mrozek <morskk@gmail.com>
2017-06-18 11:24:40 +0200

    Added bootstrapper to client

---
+++ Mors.Expenses.Client.Wpf/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Mors.Expenses.Client.Wpf
+{
+    public sealed class Bootstrapper
+    {
+        public UIElement Bootstrap()
+        {
+            return new MainPanel();
+        }
+    }
+}
